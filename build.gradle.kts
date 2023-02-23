4dfc2248dd9fe08f0109924c04d377e3457554fd
Luciannodev <joseluciano.dev@gmail.com>
2023-02-23 20:24:59 -0300

    Dockerfile

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 import groovy.xml.dom.DOMCategory.attributes
 
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+
 plugins {
 	java
 	id("org.springframework.boot") version "2.7.8"
 	id("io.spring.dependency-management") version "1.1.0"
+	kotlin("jvm") version "1.5.32"
+	kotlin("plugin.spring") version "1.5.32"
 }
 
 
@@ -38,5 +43,10 @@ dependencies {
 }
 
 
-
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
+}
 
